a5a4a9b4a64e4c30ec1f961a91640535c67a5818
Volodymyr Kravchenko <volodymyr.kravchenko.2000@gmail.com>
2018-09-15 18:00:53 +0300

    Added action for selection item

--- src/index.js
+++ src/index.js
@@ -5,33 +5,52 @@ import {createStore} from "redux";
 
 
 const ADD_ITEM="addItemAction";
+const SELECT_ITEM="selectItemAction";
 const addItemAction = (text) => {
     return {
         type: ADD_ITEM,
         value: text
     }
 }
+const selectItemAction = (item) => {
+    return {
+        type: SELECT_ITEM,
+        value: item
+    }
+}
 
 const reducerOne = (state={actions:[],lastActionIndex:0}, action)=>{
-    console.log("ACTION", state, action);
     switch(action.type) {
         case ADD_ITEM:
+            console.log("ADD ACTION ", action)
             return {
-                actions: [...state.actions, action.value]
+                actions: [...state.actions, {id:state.lastActionIndex+1, value:action.value}],
+                lastActionIndex: state.lastActionIndex+1
             }
+        case SELECT_ITEM:
+            console.log("SELECT ACTION ", action)
+            return state;
         default:
+            console.log("UNKNOWN ACTION IS DETECTED");
             return state;
     }
 }
 const store = createStore(reducerOne);
 
 export class ListOfItems extends Component {
+    onClickItem = (e)=>{
+        const clickedItem  = this.props.items.filter((item)=>{
+            return item.id == e.target.id;
+        });
+        store.dispatch(selectItemAction(clickedItem[0]));
+        console.log("CLICKED ITEM", clickedItem[0].value);
+    }
     render = () => {
         const { items } = this.props;
         return (
             <div>
                 {items.map((item)=>{
-                    return <div>{item}</div>
+                    return <div onClick={this.onClickItem} key={item.id} id={item.id}>{item.id}. {item.value}</div>
                 })}
             </div>
         )
@@ -48,7 +67,9 @@ export class App extends Component {
                         this.inputField.value = "";
                     }
                 }/>
-                <ListOfItems items={store.getState().actions}/>
+                <ListOfItems items={store.getState().actions} onSelectItem={(item)=>{
+                    store.dispatch(selectItemAction(item))
+                }}/>
             </div>
         )
     }
